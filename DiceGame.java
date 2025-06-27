ccd7dc6d3e80b3f902b71ead14b2b9a3b65de648
s1320238 <s1320238@u-aizu.ac.jp>
2025-06-27 12:20:31 +0900

    Greetings added

--- DiceGame.java
+++ DiceGame.java
@@ -1,4 +1,5 @@
-public class MainBranch {
+import java.util.Scanner;
+public class DiceGame {
   public static void main(Strings[] a) {
     run();
   }
@@ -6,9 +7,14 @@ public class MainBranch {
   public void run() {
      int a,b;
 
+    System.out.println("What is your name? ");
+    Scanner scanner = new Scanner(System.in);
+    String name = scanner.nextLine();
+    System.out.println("Hello, John!");
+    
      a=dice();
      b=dice();
-     System.out.println("dice1: "+a+"\ndice2: "+b+"\ntotal"+(a+b));
+     System.out.println("dice1: "+a+"\ndice2: "+b+"\ntotal　"+(a+b));
   }
   
   public int dice() {
