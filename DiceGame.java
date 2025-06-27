d8a1f2ec6a0e0c18c7520473aab26c40a344551b
s1320238 <s1320238@u-aizu.ac.jp>
2025-06-27 12:28:35 +0900

    Update DiceGame.java

--- DiceGame.java
+++ DiceGame.java
@@ -1,23 +1,3 @@
 import java.util.Scanner;
-public class DiceGame {
-  public static void main(Strings[] a) {
-    run();
-  }
-  
-  public void run() {
-     int a,b;
 
-    System.out.println("What is your name? ");
-    Scanner scanner = new Scanner(System.in);
-    String name = scanner.nextLine();
-    System.out.println("Hello, John!");
-    
-     a=dice();
-     b=dice();
-     System.out.println("dice1: "+a+"\ndice2: "+b+"\ntotal　"+(a+b));
-  }
-  
-  public int dice() {
-    return (int)(Math.random()*6+1);
-  }
-}
+
