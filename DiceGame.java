09069a9c4fc61af0f3100ee13f4efcf173345427
s1320238 <s1320238@u-aizu.ac.jp>
2025-06-30 16:15:04 +0900

    Won/lost message added

--- DiceGame.java
+++ DiceGame.java
@@ -1,3 +1,49 @@
+import java.util.Scanner;
+
+public class DiceGame {
+  public static void main(String[] a) {
+    run();
+  }
+  
+  public static void run() {
+     int a,b;
+     String name;
+     name=Greeting();
+
+    
+     a=dice();
+     b=dice();
+     System.out.println("dice1: "+a+"\ndice2: "+b+"\ntotal　"+(a+b));
+  
+if(a+b>=8) System.out.println("You won");
+    else System.out.println("You lost");
+
+  }
+public static int dice() {
+    return (int)(Math.random()*6+1);
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 public class DiceGame {
